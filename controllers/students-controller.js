12e7fa2c332f11e34d09c8886c5bf7bec89ae6eb
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-10-12 23:00:27 +0300

    Update home page title

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "Students Registry 1",
+      msg: "The new Students Registry",
       students: students
     };
     res.render('home', model);
